862348e8402ed82231c4422d25a3319c4272965e
jinjied <44046513+SecondChilde876@users.noreply.github.com>
2021-11-08 13:23:06 +0800

    change router mode

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import "nprogress/nprogress.css";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: "history",
+    mode: "hash",
     base: process.env.BASE_URL,
     routes: [{
             path: "/login",
